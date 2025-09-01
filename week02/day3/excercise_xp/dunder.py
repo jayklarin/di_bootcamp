97d47478dcac68930bf116ef12a9e65a6864a5c0
jayklarin <jayklarin@gmail.com>
2025-09-01 13:28:23 +0300

    GitHub requires message

--- week02/day3/excercise_xp/dunder.py
+++ week02/day3/excercise_xp/dunder.py
@@ -26,52 +26,53 @@
 
 # Instructions:
 
-# class Currency:
-#     def __init__(self, currency, amount):
-#         self.currency = currency
-#         self.amount = amount
+class Currency:
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
 
-#     #Your code starts HERE
+    #Your code starts HERE
+
 
 
 # Using the code above, implement the relevant methods and dunder methods which will output the results below.
 
 # Hint : When adding 2 currencies which don’t share the same label you should raise an error.
 
-# c1 = Currency('dollar', 5)
-# c2 = Currency('dollar', 10)
-# c3 = Currency('shekel', 1)
-# c4 = Currency('shekel', 10)
+c1 = Currency('dollar', 5)
+c2 = Currency('dollar', 10)
+c3 = Currency('shekel', 1)
+c4 = Currency('shekel', 10)
 
-# #the comment is the expected output
-# print(c1)
-# # '5 dollars'
+#the comment is the expected output
+print(c1)
+# '5 dollars'
 
-# print(int(c1))
-# # 5
+print(int(c1))
+# 5
 
-# print(repr(c1))
-# # '5 dollars'
+print(repr(c1))
+# '5 dollars'
 
-# print(c1 + 5)
-# # 10
+print(c1 + 5)
+# 10
 
-# print(c1 + c2)
-# # 15
+print(c1 + c2)
+# 15
 
-# print(c1)
-# # 5 dollars
+print(c1)
+# 5 dollars
 
-# c1 += 5
-# print(c1)
-# # 10 dollars
+c1 += 5
+print(c1)
+# 10 dollars
 
-# c1 += c2
-# print(c1)
-# # 20 dollars
+c1 += c2
+print(c1)
+# 20 dollars
 
-# print(c1 + c3)
-# # TypeError: Cannot add between Currency type <dollar> and <shekel>
+print(c1 + c3)
+# TypeError: Cannot add between Currency type <dollar> and <shekel>
 
 
 # 🌟 Exercise 2: Import
